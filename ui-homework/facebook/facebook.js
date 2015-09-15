11a42e500fcf2aeae4d4fcd78f1f6fd108e5c0a2
Kate KyuWon <lkwon0302@gmail.com>
2015-09-15 22:30:49 +0900

    haha

--- ui-homework/facebook/facebook.js
+++ ui-homework/facebook/facebook.js
@@ -3,97 +3,98 @@
 window.onscroll= function(){
 	if(document.body.scrollHeight - document.body.scrollTop < 1000) {
 
-			var newDiv = document.createElement('div');
-			newDiv.classList.add('content-box');
-			var newName = document.createElement('div');
-			newName.classList.add('content-name');
-			var newContent = document.createElement('div');
-			newContent.classList.add('content-content');
-			
-			//갯수 
-			var newCount = document.createElement('div');
-			newCount.classList.add('button-count');
+		var newDiv = document.createElement('div');
+		newDiv.classList.add('content-box');
+		var newName = document.createElement('div');
+		newName.classList.add('content-name');
+		var newContent = document.createElement('div');
+		newContent.classList.add('content-content');
+		
+		//갯수 
+		var newCount = document.createElement('div');
+		newCount.classList.add('button-count');
 
-			var newLike = document.createElement('span');
-			newLike.classList.add("like-count");
-			newLike.textContent = 2;
-			newCount.appendChild(document.createTextNode("좋아요 "));
-			newCount.appendChild(newLike);
-			newCount.appendChild(document.createTextNode('개 '));
+		var newLike = document.createElement('span');
+		newLike.classList.add("like-count");
+		newLike.textContent = 2;
+		newCount.appendChild(document.createTextNode("좋아요 "));
+		newCount.appendChild(newLike);
+		newCount.appendChild(document.createTextNode('개 '));
 
-			var newComment = document.createElement('span');
-			newComment.classList.add("comment-count");
-			newComment.textContent = 3;
-			newCount.appendChild(document.createTextNode("댓글 "));
-			newCount.appendChild(newComment);
-			newCount.appendChild(document.createTextNode('개 '));
+		var newComment = document.createElement('span');
+		newComment.classList.add("comment-count");
+		newComment.textContent = 3;
+		newCount.appendChild(document.createTextNode("댓글 "));
+		newCount.appendChild(newComment);
+		newCount.appendChild(document.createTextNode('개 '));
 
-			var newShare = document.createElement('span');
-			newShare.classList.add("share-count");
-			newShare.textContent = 4;
-			newCount.appendChild(document.createTextNode("공유 "));
-			newCount.appendChild(newShare);
-			newCount.appendChild(document.createTextNode('개 '));
-			
-			//버튼
-			var newButton = document.createElement('div');
-			newButton.classList.add('content-button');
+		var newShare = document.createElement('span');
+		newShare.classList.add("share-count");
+		newShare.textContent = 4;
+		newCount.appendChild(document.createTextNode("공유 "));
+		newCount.appendChild(newShare);
+		newCount.appendChild(document.createTextNode('개 '));
+		
+		//버튼
+		var newButton = document.createElement('div');
+		newButton.classList.add('content-button');
 
-			var likeImg = document.createElement('img');
-			likeImg.src="thumbs-up.svg";
-			var newLikeB = document.createElement('span');
-			newLikeB.classList.add("like-button");
-			newLikeB.textContent = '좋아요 ';
-			newButton.appendChild(likeImg);
-			newButton.appendChild(newLikeB);
+		var likeImg = document.createElement('img');
+		likeImg.src="thumbs-up.svg";
+		var newLikeB = document.createElement('span');
+		newLikeB.classList.add("like-button");
+		newLikeB.textContent = '좋아요 ';
+		newButton.appendChild(likeImg);
+		newButton.appendChild(newLikeB);
 
-			var commentImg = document.createElement('img');
-			commentImg.src="comment.svg";
-			var newCommentB = document.createElement('span');
-			newCommentB.classList.add("comment-button");
-			newCommentB.textContent = '댓글달기 ';
-			newButton.appendChild(commentImg);
-			newButton.appendChild(newCommentB);
+		var commentImg = document.createElement('img');
+		commentImg.src="comment.svg";
+		var newCommentB = document.createElement('span');
+		newCommentB.classList.add("comment-button");
+		newCommentB.textContent = '댓글달기 ';
+		newButton.appendChild(commentImg);
+		newButton.appendChild(newCommentB);
 
-			var shareImg = document.createElement('img');
-			shareImg.src="mail-forward.svg";
-			var newShareB = document.createElement('span');
-			newShareB.classList.add("share-button");
-			newShareB.textContent = '공유하기 ';
-			newButton.appendChild(shareImg);
-			newButton.appendChild(newShareB);
+		var shareImg = document.createElement('img');
+		shareImg.src="mail-forward.svg";
+		var newShareB = document.createElement('span');
+		newShareB.classList.add("share-button");
+		newShareB.textContent = '공유하기 ';
+		newButton.appendChild(shareImg);
+		newButton.appendChild(newShareB);
 
-			newDiv.appendChild(newName);
-			newDiv.appendChild(newContent);
-			newDiv.appendChild(newCount);
-			newDiv.appendChild(newButton);
-			
+		newDiv.appendChild(newName);
+		newDiv.appendChild(newContent);
+		newDiv.appendChild(newCount);
+		newDiv.appendChild(newButton);
+		
+		var wrapper = document.getElementById('content-wrapper');
+		wrapper.appendChild(newDiv);
 
-			var wrapper = document.getElementById('content-wrapper');
-			wrapper.appendChild(newDiv);
+			//load five content-box from json
+		console.log("check");
+	    var xobj = new XMLHttpRequest();
+	        xobj.overrideMimeType("application/json");
+	    var pageName = '';
+	    var i;
+	    for( i=1 ; i <= 5 ; i++){
+	    	pageName = 'page' + i + '.json';
+	    
+		    console.log(pageName);
+		    xobj.open('GET', pageName, true); // Replace 'my_data' with the path to your file
+		    xobj.onreadystatechange = function () {
+				if (xobj.readyState == 4 && xobj.status == "200") {
+				    var actual_JSON = JSON.parse(xobj.responseText);
+					fillContext(actual_JSON);
+				    console.log(actual_JSON);
+			    }
+		    };
+		    xobj.send(null);
+		}  
 	}
 	console.log(document.body.scrollHeight - document.body.scrollTop);
 
-	//load five content-box from json
-	console.log("check");
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    var pageName = '';
-    var i;
-    for( i=1 ; i <= 5 ; i++){
-    	pageName = 'page' + i + '.json';
-    
-	    console.log(pageName);
-	    xobj.open('GET', pageName, true); // Replace 'my_data' with the path to your file
-	    xobj.onreadystatechange = function () {
-		  if (xobj.readyState == 4 && xobj.status == "200") {
-		    var actual_JSON = JSON.parse(xobj.responseText);
-			fillContext(actual_JSON);
-		    console.log(actual_JSON);
-	      }
-	    };
-	    xobj.send(null);
-	}  
+	
 }
 
 
