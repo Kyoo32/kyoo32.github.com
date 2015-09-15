13a7de2799e7988fe5af442264f80b72fd01d21c
Kate KyuWon <lkwon0302@gmail.com>
2015-09-15 21:16:27 +0900

    jsonp?

--- ui-homework/facebook/facebook.js
+++ ui-homework/facebook/facebook.js
@@ -132,31 +132,23 @@ document.addEventListener("click", function(evt){
 });
 
 //load five content-box from json
-/*
-document.body.onscroll = function loadJSON(callback) {   
+
+window.onscroll = function(){   
 	console.log("check");
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
     xobj.open('GET', 'page1.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
+		      var actual_JSON = JSON.parse(response);
+		      console.log("actual_JSON");
+        	}
     };
     xobj.send(null);  
  }
  
-function init() {
- loadJSON(function(response) {
-  // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
-    console.log(actual_JSON);
- });
-}
-*/
 
-window.onscroll = myFunction;
+/*
 function myFunction(arr) {
 	console.log(arr);
     var out = "";
@@ -186,4 +178,4 @@ function myFunction(arr) {
     }
 }
 
-
+*/
